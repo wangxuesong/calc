482815e071abb04be6180f5d8cfa96760c4d77f8
NerdyCat <as.liying.yang@gmail.com>
2014-11-27 00:05:26 +0800

    add test

---
+++ src/testCalc/eval_test.go
@@ -0,0 +1,40 @@
+package testCalc
+
+import "testing"
+import "eval"
+
+func TestMapping (t *testing.T){
+    var str string = "1+1"
+    ret := eval.Mapping(str)
+    if 2 != ret {
+    t.Errorf("eval.Mapping(\"1+1\") failed. Get %d, except 2", ret)
+    }
+}
+func TestMapping1 (t *testing.T){
+    var str string = "1+2"
+    ret := eval.Mapping(str)
+    if 3 != ret {
+    t.Errorf("eval.Mapping(\"1+2\") failed. Get %d, except 3", ret)
+    }
+}
+func TestMapping2 (t *testing.T){
+    var str string = "2+1"
+    ret := eval.Mapping(str)
+    if 3 != ret {
+    t.Errorf("eval.Mapping(\"2+1\") failed. Get %d, except 3", ret)
+    }
+}
+func TestMapping3 (t *testing.T){
+    var str string = "2+2"
+    ret := eval.Mapping(str)
+    if 4 != ret {
+    t.Errorf("eval.Mapping(\"2+2\") failed. Get %d, except 4", ret)
+    }
+}
+func TestMapping4 (t *testing.T){
+    var str string = "testttt"
+    ret := eval.Mapping(str)
+    if -1 != ret {
+    t.Errorf("eval.Mapping(\"testttt\") failed. Get %d, except -1", ret)
+    }
+}
