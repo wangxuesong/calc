7698e77e7d866ca04db0c44e746598764684a6ab
NerdyCat <as.liying.yang@gmail.com>
2014-11-27 22:34:51 +0800

    modify fomat by go fmt and change unit test path

--- src/calc/calc.go
+++ src/calc/calc.go
@@ -1,21 +1,22 @@
 package main
+
 import "fmt"
 import "os"
 import "read"
 import "eval"
 
-func main(){
-    args := read.Checkinput(os.Args)
-    if "" == args {
-        fmt.Println("usage: ./calc <expression>")
-        return
-    }
-    fmt.Println("input string: ",args);
+func main() {
+	args := read.Checkinput(os.Args)
+	if "" == args {
+		fmt.Println("usage: ./calc <expression>")
+		return
+	}
+	fmt.Println("input string: ", args)
 
-    var ret int = eval.Mapping(args)
-    if (-1 == ret){
-        fmt.Println("please try again!")
-        return
-    }
-    fmt.Println(os.Args[1],"=",ret)
+	var ret int = eval.Mapping(args)
+	if -1 == ret {
+		fmt.Println("please try again!")
+		return
+	}
+	fmt.Println(os.Args[1], "=", ret)
 }
