af2846fb42c4e75c1cae5ef11b8acef36af9bb34
NerdyCat <as.liying.yang@gmail.com>
2014-12-01 23:55:42 +0800

    fix bug by moving test files

--- src/read/checkinput.go
+++ src/read/checkinput.go
@@ -3,13 +3,13 @@ package read
 import "fmt"
 
 func Checkinput(Args []string) string {
-    if len(Args) == 1{
-        fmt.Println("please input one expression!")
-        return ""
-    }
-    if len(Args) > 2{
-        fmt.Println("Don't input more than one expression!")
-        return ""
-    }
-    return Args[1]
+	if len(Args) == 1 {
+		fmt.Printf("please input one expression!")
+		return ""
+	}
+	if len(Args) > 2 {
+		fmt.Println("Dont't input more than one expression!")
+		return ""
+	}
+	return Args[1]
 }
--- src/read/checkinput_test.go
+++ src/read/checkinput_test.go
@@ -1,25 +1,24 @@
-package testCalc
+package read
 
-import "read"
 import "testing"
 
 func TestCheckinput(t *testing.T) {
 	args := []string{"./calc", "1+1"}
-	ret := read.Checkinput(args)
+	ret := Checkinput(args)
 	if ret != args[1] {
 		t.Errorf("Checkinput failed. Get %s, except %s", ret, args[1])
 	}
 }
 func TestCheckinput1(t *testing.T) {
 	args := []string{"./calc"}
-	ret := read.Checkinput(args)
+	ret := Checkinput(args)
 	if ret != "" {
 		t.Errorf("Checkinput failed. Get %s, except \"\" ", ret)
 	}
 }
 func TestCheckinput2(t *testing.T) {
 	args := []string{"./calc", "1+1", "ddd"}
-	ret := read.Checkinput(args)
+	ret := Checkinput(args)
 	if ret != "" {
 		t.Errorf("Checkinput failed. Get %s, except \"\" ", ret)
 	}
